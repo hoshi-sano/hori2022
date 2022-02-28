4e03d6308b6ce2a3b238a344ef58cecf4e3c3799
Yuki Morohoshi <hoshi.sanou@gmail.com>
2022-03-01 02:02:36 +0900

    feat: タイル移動のアニメーション実装

--- src/ph_bang_bang/blank_tile.rb
+++ src/ph_bang_bang/blank_tile.rb
@@ -8,6 +8,18 @@ class PhBangBang::BlankTile < PhBangBang::Tile
     end
   end
 
+  def tx=(next_x)
+    super
+    # 通常のタイルと異なり、即時に指定位置へ移動する
+    self.x = @field.x + @tx * WIDTH
+  end
+
+  def ty=(next_y)
+    super
+    # 同上
+    self.y = @field.y + @ty * HEIGHT
+  end
+
   def hit
   end
 end
--- src/ph_bang_bang/tile.rb
+++ src/ph_bang_bang/tile.rb
@@ -3,6 +3,8 @@ class PhBangBang::Tile < PhBangBang::Sprite
   WIDTH = 80
   HEIGHT = 80
   LINE_W = 2
+  X_SPEED = WIDTH / 4
+  Y_SPEED = HEIGHT / 4
 
   attr_reader :tx, :ty
 
@@ -20,19 +22,51 @@ class PhBangBang::Tile < PhBangBang::Sprite
     @tx = x
     @ty = y
     @field = field
+    @moving = false
+    @dest_x = nil
+    @dest_y = nil
   end
 
   def tx=(next_x)
     @tx = next_x
-    self.x = @field.x + @tx * WIDTH
+    @moving = true
+    @dest_x = @field.x + @tx * WIDTH
   end
 
   def ty=(next_y)
     @ty = next_y
-    self.y = @field.y + @ty * HEIGHT
+    @moving = true
+    @dest_y = @field.y + @ty * HEIGHT
+  end
+
+  def update
+    super
+    update_x
+    update_y
+  end
+
+  def update_x
+    return unless (@moving && @dest_x)
+    d = self.x > @dest_x ? -1 : 1
+    self.x += d * X_SPEED
+    if self.x == @dest_x
+      @dest_x = nil
+      @moving = false
+    end
+  end
+
+  def update_y
+    return unless (@moving && @dest_y)
+    d = self.y > @dest_y ? -1 : 1
+    self.y += d * Y_SPEED
+    if self.y == @dest_y
+      @dest_y = nil
+      @moving = false
+    end
   end
 
   def hit(other)
+    return if @moving
     return unless other.is_a?(PBB::TouchCircle)
     @field.move(self)
   end
